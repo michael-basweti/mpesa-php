7d661ab619f54e5178c9fa9950b5647a6b47e709
michael-basweti <baswetima@gmail.com>
2020-03-04 02:06:00 +0300

    change

--- registerurl.php
+++ registerurl.php
@@ -1,32 +1,25 @@
 <?php
-    $consumerKey = 'DPh667WXtdmPHs4OhodoOgYCGPPjdh2m';
-    $consumerSecret = 'mzxzOYIA5zGKJmvN';
-    $headers = ['Content-Type:application/json','charset-utf8'];
-
-    $url = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl';
-
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers); //setting a custom header
+    $url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+  
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    $credentials = base64_encode('DPh667WXtdmPHs4OhodoOgYCGPPjdh2m:mzxzOYIA5zGKJmvN');
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic '.$credentials)); //setting a custom header
     curl_setopt($curl, CURLOPT_HEADER, false);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_USRPWD, $consumerKey.':'.$consumerSecret);
-
-    $result = curl_exec($curl);
-    $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    $result = json_decode($result);
-
-
-    $access_token=$result->access_token;
-
-    echo($access_token);
-
-    curl_close($curl);
-
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    
+    $curl_response = curl_exec($curl);
+    
+    // echo ;
+    // echo(json_decode($curl_response)->access_token);
+    $tel = json_decode($curl_response)->access_token;
+    // echo($tel);
  
   
   $curl = curl_init();
   curl_setopt($curl, CURLOPT_URL, $url);
-  curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$access_token1)); //setting custom header
+  curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$tel)); //setting custom header
   
   
   $curl_post_data = array(
