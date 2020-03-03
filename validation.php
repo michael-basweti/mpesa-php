007a823ede197fb59ed783bca0d311cc12c88f1b
michael-basweti <baswetima@gmail.com>
2020-03-03 16:39:49 +0300

    message

--- validation.php
+++ validation.php
@@ -3,11 +3,11 @@
     $response = '{
         "ResultCode":0,
         "ResultDesc": "Configuration received successfully"
-    }'
+    }';
     // Data
     $mpesaResponse = file_get_contents('php://input');
 
-    $logFile = "M_PesaResponse.txt"
+    $logFile = "M_PesaResponse.txt";
 
     $jsonMpesaResponse = json_decode($mpesaResponse, true);
 
