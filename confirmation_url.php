4564d9bded2709424ef149bf2df3671458a09ec5
michael-basweti <baswetima@gmail.com>
2020-03-03 16:06:44 +0300

    make it better

---
+++ confirmation_url.php
@@ -0,0 +1,21 @@
+<?php
+    header("Content-Type: application/json");
+    $response = '{
+        "ResultCode":0,
+        "ResultDesc": "Configuration received successfully"
+    }'
+    // Data
+    $mpesaResponse = file_get_contents('php://input');
+
+    $logFile = "M_PesaResponse.txt"
+
+    $jsonMpesaResponse = json_decode($mpesaResponse, true);
+
+    // write to file
+    $log = fopen($logFile, "a");
+
+    fwrite($log, $jsonMpesaResponse);
+    fclose($log);
+
+    echo $response
+?>
