36310433d4f8d825b758de1d0ed4e7849e1051f9
michael-basweti <baswetima@gmail.com>
2020-03-03 17:23:12 +0300

    add access token

--- registerurl.php
+++ registerurl.php
@@ -9,13 +9,13 @@
 
     $curl_response = curl_exec($curl);
 
-    echo json_decode($curl_response)->access_token;
+    $access_token=json_decode($curl_response)->access_token;
 
   $url = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl';
   
   $curl = curl_init();
   curl_setopt($curl, CURLOPT_URL, $url);
-  curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer ' .$access_token)); //setting custom header
+  curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$access_token)); //setting custom header
   
   
   $curl_post_data = array(
