f69b7bb0a390c83782433ff6f055c03d2e8bb5e0
michael-basweti <baswetima@gmail.com>
2020-03-04 11:52:54 +0300

    la

--- registerurl.php
+++ registerurl.php
@@ -13,7 +13,9 @@
     
     // echo ;
     // echo(json_decode($curl_response)->access_token);
-    $tel = json_decode($curl_response1)->access_token;
+
+    $ann = json_decode($curl_response1);
+    $tel = $ann->access_token;
     echo($tel);
  
     $url1 = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl';
